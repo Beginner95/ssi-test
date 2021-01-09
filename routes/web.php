e37e7673571dfcc64eaaf5e4b2c68c49f64a7d0a
Vaharsolta <vaha-22@mail.ru>
2021-01-10 01:30:52 +0300

    Добавил базовые роту, главная страница админ панели, авторизация и выход из системы

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\MainController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+    Route::get('/', [MainController::class, 'index'])->name('admin.index');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/admin/login', [UserController::class, 'loginForm'])->name('login.create');
+    Route::post('/admin/login', [UserController::class, 'login'])->name('login');
+});
+
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
