df00881c170f0b8b79f614f2af09a7ad0bcceb78
Vaharsolta <vaha-22@mail.ru>
2021-01-10 08:46:40 +0300

    Добавлена ссылка на страницу моделей

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -57,7 +57,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{ route('car-models.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-copy"></i>
                             <p>Модели</p>
                         </a>
