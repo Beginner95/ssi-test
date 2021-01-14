edb9fb8169f2d95e1af1e811bc1391656955f291
Vaharsolta <vaha-22@mail.ru>
2021-01-14 20:42:44 +0300

    Удалил Request class из подключения

--- app/Http/Controllers/Admin/CarController.php
+++ app/Http/Controllers/Admin/CarController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\CarRequest;
 use App\Models\Car;
 use App\Models\CarModel;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class CarController extends Controller
